66b0ee4435c3d339ca11a88a0a7b3fc67654c163
Zach Hammer <zhammer@seatgeek.com>
2024-12-02 15:57:42 -0500

    first passing test

--- analyzers/slogcontext/analyzer.go
+++ analyzers/slogcontext/analyzer.go
@@ -1,10 +1,14 @@
 package slogcontext
 
 import (
+	"fmt"
+	"go/ast"
+
 	"github.com/seatgeek/sgmods-go/pkg/util"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/ctrlflow"
 	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 const slogPackage = "log/slog"
@@ -18,6 +22,57 @@ var SlogContextAnalyzer = &analysis.Analyzer{
 			return nil, nil
 		}
 
+		nodeFilter := []ast.Node{
+			(*ast.CallExpr)(nil),
+		}
+
+		inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+		inspector.WithStack(nodeFilter, func(node ast.Node, push bool, stack []ast.Node) bool {
+			callExpr, ok := node.(*ast.CallExpr)
+			if !ok {
+				panic(fmt.Sprintf("unexpected node type %T", node))
+			}
+
+			slogCall, ok := determineSlogCall(callExpr)
+			if !ok {
+				return false
+			}
+
+			switch slogCall {
+			case "Debug", "Info", "Warn", "Error":
+				break
+			default:
+				return false
+			}
+
+			pass.Report(analysis.Diagnostic{
+				Pos:     node.Pos(),
+				End:     node.End(),
+				Message: "context not passed to slog call",
+			})
+
+			return false
+		})
+
 		return nil, nil
 	},
+}
+
+// returns the slog call name, false if not an slog call
+func determineSlogCall(callExpr *ast.CallExpr) (string, bool) {
+	selector, ok := callExpr.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return "", false
+	}
+
+	x, ok := selector.X.(*ast.Ident)
+	if !ok {
+		return "", false
+	}
+
+	if x.Name != "slog" {
+		return "", false
+	}
+
+	return selector.Sel.Name, true
 }
--- analyzers/slogcontext/testdata/src/basic/basic.go
+++ analyzers/slogcontext/testdata/src/basic/basic.go
@@ -10,8 +10,8 @@ func main() {
 }
 
 func run(ctx context.Context) {
-	slog.Debug("debug msg") // want `context.Context not passed to slog.Debug`
-	slog.Info("info msg")   // want `context.Context not passed to slog.Info`
-	slog.Warn("warn msg")   // want `context.Context not passed to slog.Warn`
-	slog.Error("error msg") // want `context.Context not passed to slog.Error`
+	slog.Debug("debug msg") // want `context not passed to slog call`
+	slog.Info("info msg")   // want `context not passed to slog call`
+	slog.Warn("warn msg")   // want `context not passed to slog call`
+	slog.Error("error msg") // want `context not passed to slog call`
 }

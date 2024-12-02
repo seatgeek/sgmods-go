cabd2a18e33a248681f359ff9bc1a8dcf222f73d
Zach Hammer <zhammer@seatgeek.com>
2024-12-02 16:37:35 -0500

    add mod to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/seatgeek/sgmods-go/analyzers/slogcontext"
 	"github.com/seatgeek/sgmods-go/analyzers/wrap_error"
 
 	"golang.org/x/tools/go/analysis/multichecker"
 )
 
 func main() {
-	multichecker.Main(wrap_error.WrapErrorAnalyzer)
+	multichecker.Main(wrap_error.WrapErrorAnalyzer, slogcontext.SlogContextAnalyzer)
 }

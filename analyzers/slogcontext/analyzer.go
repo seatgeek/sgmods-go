e3074579f35642e5bbe6309a998efc55dcc0f059
Zach Hammer <zhammer@seatgeek.com>
2024-12-02 16:40:54 -0500

    remove ctrlflow pass

--- analyzers/slogcontext/analyzer.go
+++ analyzers/slogcontext/analyzer.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/seatgeek/sgmods-go/pkg/util"
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/ctrlflow"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
@@ -16,7 +15,7 @@ const slogPackage = "log/slog"
 var SlogContextAnalyzer = &analysis.Analyzer{
 	Name:     "slogcontext",
 	Doc:      "check that context is passed to all slog calls",
-	Requires: []*analysis.Analyzer{inspect.Analyzer, ctrlflow.Analyzer},
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 	Run: func(pass *analysis.Pass) (interface{}, error) {
 		if !util.Imports(pass.Pkg, slogPackage) {
 			return nil, nil
